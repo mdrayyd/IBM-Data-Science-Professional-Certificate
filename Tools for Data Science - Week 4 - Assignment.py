a7e14e967d74a9be440f51c9748be69bd3006af0
Made Ray Yuda Suyatna <71420761+mdrayyd@users.noreply.github.com>
2022-01-30 17:43:17 +0800

    Add files via upload

---
+++ Tools for Data Science - Week 4 - Assignment.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **Made Ray Yuda Suyatna**
+
+# Banker
+
+# *I am interested in data science because I love to gain insights from data*
+
+# ### The following code tests the Gauss formula
+
+# In[1]:
+
+
+def gauss(n):
+    return (n*(n+1))/2
+
+gauss(100) == sum(range(101))
+
+
+# ---
+# 
+# + one
+# + two
+# + three
+# + one
+# + two
+# + three
+# 
+# Markdown | Less | Pretty
+# --- | --- | ---
+# *Still* | `renders` | **nicely**
+# 1 | 2 | 3
+
+# In[ ]:
+
+
+
+
